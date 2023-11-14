aa02d4d1ec34a4865e067ec6d5a6b88e4e91204a
Maksim Makaranka <maksim.makaranka@eracent.com>
2023-11-14 16:50:35 +0100

    Add (a) task report

--- src/core.py
+++ src/core.py
@@ -6,14 +6,14 @@ import datetime as dt
 class DataLoader:
     def __init__(self, data_path):
         df = pd.read_csv(path.join(data_path, 'unrate_adjusted.csv'))
-        df.columns = ['Date', 'Period', 'Rate']
-        df['Date'] = pd.to_datetime(df['Date'])
-        df = df[(df['Date'] > dt.datetime(1997, 1, 1)) & (df['Date'] < dt.datetime(2023, 10, 1))]
+        df.columns = ['CollectionDate', 'Period', 'Rate']
+        df['CollectionDate'] = pd.to_datetime(df['CollectionDate'])
+        df = df[(df['CollectionDate'] > dt.datetime(1997, 1, 1)) & (df['CollectionDate'] < dt.datetime(2023, 10, 1))]
         self.unrate_adjusted_df = df
 
         if path.exists(path.join(data_path, 'unrate_unadjusted.csv')):
             df = pd.read_csv(path.join(data_path, 'unrate_unadjusted.csv'))
-            df.columns = ['Date', 'Period', 'Rate']
-            df['Date'] = pd.to_datetime(df['Date'])
-            df = df[(df['Date'] > dt.datetime(1997, 1, 1)) & (df['Date'] < dt.datetime(2023, 10, 1))]
+            df.columns = ['CollectionDate', 'Period', 'Rate']
+            df['CollectionDate'] = pd.to_datetime(df['CollectionDate'])
+            df = df[(df['CollectionDate'] > dt.datetime(1997, 1, 1)) & (df['CollectionDate'] < dt.datetime(2023, 10, 1))]
             self.unrate_unadjusted_df = df
