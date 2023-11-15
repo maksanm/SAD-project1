616de6216eade922f84e6630b272f191a3d3ba1c
Maksim Makaranka <maksim.makaranka@eracent.com>
2023-11-15 23:24:50 +0100

    Refactor data loading, add Poland male and female data import

--- src/core.py
+++ src/core.py
@@ -5,15 +5,17 @@ import datetime as dt
 
 class DataLoader:
     def __init__(self, data_path):
-        df = pd.read_csv(path.join(data_path, 'unrate_adjusted.csv'))
-        df.columns = ['CollectionDate', 'Period', 'Rate']
-        df['CollectionDate'] = pd.to_datetime(df['CollectionDate'])
-        df = df[(df['CollectionDate'] > dt.datetime(1997, 1, 1)) & (df['CollectionDate'] < dt.datetime(2023, 10, 1))]
-        self.unrate_adjusted_df = df
+        self.unrate_adjusted_df = self.get_unrate_df_from_csv(data_path, 'unrate_adjusted.csv')
+        self.unrate_unadjusted_df = self.get_unrate_df_from_csv(data_path, 'unrate_unadjusted.csv')
+        self.unrate_unadjusted_male_df = self.get_unrate_df_from_csv(data_path, 'unrate_unadjusted_male.csv')
+        self.unrate_unadjusted_female_df = self.get_unrate_df_from_csv(data_path, 'unrate_unadjusted_female.csv')
 
-        if path.exists(path.join(data_path, 'unrate_unadjusted.csv')):
-            df = pd.read_csv(path.join(data_path, 'unrate_unadjusted.csv'))
+    def get_unrate_df_from_csv(self, data_path, file_name) -> pd.DataFrame:
+        if path.exists(path.join(data_path, file_name)):
+            df = pd.read_csv(path.join(data_path, file_name))
             df.columns = ['CollectionDate', 'Period', 'Rate']
             df['CollectionDate'] = pd.to_datetime(df['CollectionDate'])
             df = df[(df['CollectionDate'] > dt.datetime(1997, 1, 1)) & (df['CollectionDate'] < dt.datetime(2023, 10, 1))]
-            self.unrate_unadjusted_df = df
+            return df
+        else:
+            return None
